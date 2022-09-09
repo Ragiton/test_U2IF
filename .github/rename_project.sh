20cb343b5a164d3fca931cf51fb48d368db3ae0a
Ragiton <Ragiton@users.noreply.github.com>
2022-09-09 20:15:50 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Ragiton"
+original_name="test_u2if"
+original_urlname="test_U2IF"
+original_description="Awesome test_u2if created by Ragiton"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv test_u2if $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from test_u2if.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "test_u2if"
